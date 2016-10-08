1dd8c35e1dddfbd109e11c58171a348b085acf59
mknittel <mknittel@g.hmc.edu>
2016-10-07 20:26:31 -0700

    Added fading text

---
+++ assets/js/ie/mknittel.js
@@ -0,0 +1,23 @@
+$("res").hover(function() {
+    document.getElementById('magic_text').innerHTML = "res";
+}, function() {
+    document.getElementById('magic_text').innerHTML = "";
+});
+
+$("fb").hover(function() {
+    document.getElementById('magic_text').innerHTML = "fb";
+}, function() {
+    document.getElementById('magic_text').innerHTML = "";
+});
+
+$("gh").hover(function() {
+    document.getElementById('magic_text').innerHTML = "gh";
+}, function() {
+    document.getElementById('magic_text').innerHTML = "";
+});
+
+$("email").hover(function() {
+    document.getElementById('magic_text').innerHTML = "email";
+}, function() {
+    document.getElementById('magic_text').innerHTML = "";
+});
